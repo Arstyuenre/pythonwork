cce66e275286916a395a8728d0c961696e3243e3
Arstyuenre <2369566514@qq.com>
2021-06-04 14:09:29 +0800

    任务1

---
+++ python作业提交/task 1.py
@@ -0,0 +1,39 @@
+import time
+import functools
+
+
+def log(text=''):
+    def metric(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            t_begin = time.time()
+            print('%s 开始于 %0.4f' % (fn.__name__, t_begin))
+            res = fn(*args, **kwargs)
+            t_end = time.time()
+            print('%s 结束于 %0.4f' % (fn.__name__, t_end))
+            print('%s%s 于 %s ms 内运行完毕' % (text, fn.__name__, t_end - t_begin))
+            return res
+
+        return wrapper
+
+    return metric
+
+
+@log()
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+
+@log()
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+
+f = fast(10, 20)
+s = slow(10, 20, 30)
+if f != 30:
+    print('测试失败!')
+elif s != 6000:
+    print('测试失败!')
